4fd74737cc4e71e79036f3d51b7ce41538ecfd56
Robert Murray <robertmurray@Roberts-iMac.local>
2015-11-26 15:09:50 +1100

    Additional styling and effects added

--- js/gui.js
+++ js/gui.js
@@ -20,6 +20,30 @@ $(document).ready(function() {
       },
       buttonDisplay : 'stacked'
     });
+
+    var winnerModal = function () {
+      var winnerTitle = "THE WINNER IS: " + winner;
+      new $.flavr({
+          title  : winnerTitle,
+          content  : 'Would you like to play again?',
+          iconPath : 'images/',
+          icon     : 'star.png',
+          buttons  : {
+          pvpButton  : { text: 'Yes, play opponant again', style: 'primary', addClass: 'submit-btn', 
+                      action: function(){
+                          playerVsPlayer();
+                      }
+          },
+          pvcButton  : { text: 'No, go to the main menu', style: 'danger', addClass: 'submit-btn', 
+                      action: function(){
+                          playerVsComputer();
+                      }
+          }
+        },
+        buttonDisplay : 'stacked'
+      });
+    };
+
     var $getBoardAndText = $("#currentPlayerH1, .board");
     $getBoardAndText.css('visibility', 'hidden');
     var executePlayerMove = function () {
@@ -30,37 +54,45 @@ $(document).ready(function() {
         $(this).off();
     };
 
+    var getPlayerColor = function () {
+      if (currentPlayer === "X") {
+        return '#fb6e52';
+      } else {
+        return '#399c95';
+      }
+    };
+
+    var getWinnerColor = function () {
+      if (winner === "X") {
+        return '#fb6e52';
+      } else {
+        return '#399c95';
+      }
+    };
+
     var executeComputerMove = function () {
         var setComputerMove = computerMove();
         var computerMoveToID = '#' + setComputerMove;
-
         placePlayerPiece(currentPlayer, setComputerMove);
-
-        // var addtext = function () {
-        //   console.log(currentPlayer);
-        //   $(computerMoveToID).text(currentPlayer);
-        //   $(computerMoveToID).addClass("tileSelected");
-        // };
-        // setTimeout(addtext, 2000);
-
         $(computerMoveToID).text(currentPlayer);
         $(computerMoveToID).addClass("tileSelected");
-        
-        (currentPlayer === "X") ? $(computerMoveToID).css('color', '#fb6e52') : $(computerMoveToID).css('color', '#399c95');
+        $(computerMoveToID).css('color', getPlayerColor());
         $(computerMoveToID).off();
     };
 
     var checkWinsGUIevents = function () {
         if (checkWins()) {
-          $("#winnerDisplay").text("Winning Player: " + winner + " " + winnerSet);
           $(".tile").off();
           var winnerSetToID = winnerSet.map(function(c) { 
             return '#' + c; } ).join(', ');
           $(".tile").removeClass("tileSelected");
-          $(".tile").addClass('tileLoser');
+          $(".tile").toggleClass('tileLoser');
           $(winnerSetToID).addClass("tileSelected");
+          $("body").css('background', getWinnerColor());
+          setTimeout(function(){ winnerModal() }, 2200);
+          setTimeout(function(){ $("body").css('background', '#2e3846') }, 1000);
+          setTimeout(function(){ $getBoardAndText.fadeOut(1000) }, 1000);
         } else {
-          $("#winnerDisplay").text("No winner yet.");
           switchPlayer();
           $("#playerDisplay").text(currentPlayer);
         }
